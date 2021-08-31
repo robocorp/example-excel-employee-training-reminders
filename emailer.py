f4ffc921efa5a69f03c8ae270673ecc9d2d3d49b
Jani Palsamäki <jani@robocorp.com>
2021-08-31 12:34:44 +0300

    RPA.Robocloud.Secrets -> RPA.Robocorp.Vault, refactor

--- emailer.py
+++ emailer.py
@@ -1,13 +1,11 @@
 from RPA.Email.ImapSmtp import ImapSmtp
-from RPA.Robocloud.Secrets import Secrets
-
-secrets = Secrets()
-secret = secrets.get_secret("emailCredentials")
-gmail_account = secret["username"]
-gmail_password = secret["password"]
+from RPA.Robocorp.Vault import Vault
 
 
 def send_email(recipient, subject, body):
+    secret = Vault().get_secret("emailCredentials")
+    gmail_account = secret["username"]
+    gmail_password = secret["password"]
     mail = ImapSmtp(smtp_server="smtp.gmail.com", smtp_port=587)
     mail.authorize(account=gmail_account, password=gmail_password)
     mail.send_message(
